1be8bc9a4522c592334abddbc3ea2ce77e265e75
Mink7891 <60268613+Mink7891@users.noreply.github.com>
2024-12-06 10:59:39 +0300

    Delete demo directory

--- demo/src/main/java/com/example/demo/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import java.util.Optional;
-
-public interface UserService {
-    Optional<UserEntity> getUser(int id);
-    UserEntity addUser(UserRequest userRequest);
-}
-
